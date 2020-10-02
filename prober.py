e964418f749ecc319200b91970759d9691c01490
yebh17 <sunnyrebels0@gmail.com>
2020-10-02 17:02:53 +0200

    Validates User Input

--- prober.py
+++ prober.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python
 import sys
-
+from easysnmp import Session,exceptions
 def main(argv):
-    print("command line arguments",argv)
+    if len(argv) == 4:
+        print("arguments length passes")
+        sys_var = argv[0].split(':')
+        if len(sys_var) == 3:
+            agent_ip = sys_var[0]
+            port= sys_var[1]
+            community = sys_var[2]
+            sample_freq = argv[1]
+            samples = argv[2]
+            oid = argv[3]
+            snmp_prober(agent_ip,port,community,sample_freq,samples,oid)
+        else:
+            print("Invalid System details <Agent_Ip:Port:Community>",len(sys_var))
+            sys.exit()
+    else:
+        print("Invalid Input Format",len(argv))
+        sys.exit()
     
 
-
+def snmp_prober(agent_ip,port,community,sample_freq,samples,oid):
+    x = []
+    y = []
+    
+    print(agent_ip,port,community,sample_freq,samples,oid) 
 
 if __name__ == "__main__":
-   main(sys.argv[1]).split(':')
+   main(sys.argv[1:])
